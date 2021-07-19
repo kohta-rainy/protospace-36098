0551040b7d6f17c15cddf93f32ce7553686f4a2a
kohta-rainy <artgc4663@gmail.com>
2021-07-19 18:01:11 +0900

    データベース変更2

--- db/migrate/20210713120755_change_datatype_profile_of_user.rb
+++
@@ -1,8 +0,0 @@
-class ChangeDatatypeProfileOfUser < ActiveRecord::Migration[6.0]
-  def change
-    change_column :users, :profile, :text
-    change_column :users, :position,:text
-    change_column :users, :occupation,:text
-
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -58,8 +58,8 @@ ActiveRecord::Schema.define(version: 2021_07_18_035807) do
     t.string "encrypted_password", default: "", null: false
     t.string "name", null: false
     t.text "profile", null: false
+    t.text "occupation", null: false
     t.text "position", null: false
-    t.text "belong", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
